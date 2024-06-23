b6a65b008be74d88ded2eb467e35378aa4610da2
Jonhy <joao-morrudo@hotmail.com.br>
2024-06-23 13:12:18 -0300

    update more than 2048

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Number : React.FC<Props> = (props) => {
     return (
-        <div className={`number n${props.value}`}>
+        <div className={`number n${props.value} ${parseInt(props.value) > 2048 ? "small" : ""}`}>
             <span>{props.value}</span>
         </div>
     )
