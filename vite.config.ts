8897719cf8b84bbe31430c506859d4d508d196b7
Jonhy <joao-morrudo@hotmail.com.br>
2024-07-17 15:13:01 -0300

    update main

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/React-Projects",
+  root: "./",
+  build: {
+    outDir: "../gh-page",
+    emptyOutDir: true,
+  }
 })
